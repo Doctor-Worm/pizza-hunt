fcc8030d859676744f504b6ddbbbccd4a038d43a
Skylar Jackson <skylarjackson@Skylars-Air.fios-router.home>
2022-07-03 17:36:10 -0400

    commiting

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,11 +3,8 @@ const { Pizza, Comment } = require('../models');
 const commentController = {
     // add Comment to Pizza
     addComment({ params, body }, res) {
-        console.log(body);
-        console.log(params);
         Comment.create(body)
             .then(({ _id }) => {
-                console.log(_id);
                 return Pizza.findOneAndUpdate(
                     { _id: params.pizzaId },
                     { $push: { comments: _id } },
@@ -22,6 +19,22 @@ const commentController = {
                 res.json(dbPizzaData);
             })
             .catch(err => res.json(err));
+    },
+    // add reply to a comment
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
     },
     // delete Comment from Pizza
     removeComment({ params }, res) {
@@ -43,6 +56,16 @@ const commentController = {
             }
             res.json(dbPizzaData);
         })
+        .catch(err => res.json(err));
+    },
+    // delete Reply from a Comment
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.json(err));
     }
 };
